69c6e7befac613c10ffb471f6403d0d64864792b
Brook Queree <brook.queree@origin.com.au>
2023-12-03 13:36:27 +1000

    Add a setup.py

---
+++ cli/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='serviciser',
+    version='0,0,1',
+    packages=find_packages(),
+    install_requires=[
+        'inquirer==3.1.4',
+    ],
+    entry_points={
+        'console_scripts': [
+            'your_script_name = cli:main',
+        ],
+    }
+)
